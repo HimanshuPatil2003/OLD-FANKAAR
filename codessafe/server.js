758dfcca3ba7b41a80dce9e5379deb3df60d34c7
HimanshuPatil2003 <97457257+HimanshuPatil2003@users.noreply.github.com>
2023-03-23 08:37:55 +0530

    Add files via upload

---
+++ codessafe/server.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+const path=require("path")
+
+app.use(express.json());
+app.set("view engine","hbs");
+app.set("views",)
+
+app.get("/", (req,res)=>{
+    res.render("login");
+})
+// app.get("/signup", (req,res)=>{
+//     res.render("signup ");
+// })
+
+app.listen(3000,()=>{
+    console.log("port connected on 3000"); 
+})
